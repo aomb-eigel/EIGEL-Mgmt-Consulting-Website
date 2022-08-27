f8e7ee5d6f23bdf36624cfccb0c2edd54120f42c
Anton Bondoc <antonbondoc14@gmail.com>
2022-08-27 12:21:47 +0800

    Finished homepage
    Co-authored-by: Angelo Louis L. Hizon <angelohizon-coder@users.noreply.github.com>

---
+++ js/search-filter-clients-and-projects/filter.js
@@ -0,0 +1,22 @@
+var input = document.getElementById("filter-project");
+input.addEventListener("input", myFunction);
+
+function myFunction(e) {
+var filter = e.target.value.toUpperCase();
+
+var list = document.getElementById("list");
+var divs = list.getElementsByTagName("div");
+for (var i = 0; i < divs.length; i++) {
+        var a = divs[i].getElementsByTagName("a")[0];
+        console.log(a)
+                    
+        if (a) {
+            if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                divs[i].style.display = "";
+            } else {
+                divs[i].style.display = "none";
+            }
+        }       
+	}
+
+}
