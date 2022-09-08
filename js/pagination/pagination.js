c958303e700327a16ac1b6b71cebce66a6312089
Anton Bondoc <antonbondoc14@gmail.com>
2022-09-08 11:55:01 +0800

    Polishing projects, decided layout for services page
    Co-authored-by: Angelo Louis L. Hizon

--- js/pagination/pagination.js
+++ js/pagination/pagination.js
@@ -2,7 +2,7 @@ const paginationNumbers = document.getElementById("pagination-numbers");
 const paginatedList = document.getElementById("articles");
 const listItems = paginatedList.querySelectorAll("div");
 
-const paginationLimit = 54;
+const paginationLimit = 45;
 const pageCount = Math.ceil(listItems.length / paginationLimit);
 let currentPage = 1;
 
